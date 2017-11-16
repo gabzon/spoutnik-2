a8b166606e1878f691009d6b83f0540922c13747
gabzon <gab.zambrano@gmail.com>
2017-11-16 22:30:47 +0100

    fixed home todays film

--- resources/views/today-films.blade.php
+++ resources/views/today-films.blade.php
@@ -3,98 +3,97 @@
 @if ($todays_movies)
   <div class="ui grid stackable desktop only">
     @foreach ($todays_movies as $movie)
-      <div class="row justify-content-md-center">
-        <div class="col-12 col-sm-12 col-md-11 col-lg-10">
-
-          <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-4">
-              <h1>{{ $movie['hour'] }}</h1>
-              <a href="{{ esc_url($movie['link']) }}" style="color:black">
-                <h1>{{ $movie['title'] }}</h1>
-              </a>
-            </div>
-            <div class="col-12 col-sm-6 col-md-8 col-lg-8"></div>
-          </div>
-
-          <div class="row">
-            <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-              <p>{{ $movie['excerpt'] }}</p>
-              <a href="{{ esc_url($movie['link']) }}" class="landing-info">{{ __('More info','sage')}}</a>
-              <br><br>
-              <h5>
-                @if ($movie['director'])
-                  {{ __('Director: ','sage') }}
-                  @foreach ($movie['director'] as $x)
-                    {{ $x->name . ' ' }}
-                  @endforeach
-                  <br>
-                @endif
-
+      <div class="one wide column"></div>
+<div class="fourteen wide column">
+    <div class="ui grid">
+        <div class="six wide column">
+            <h1><?php echo $movie['hour']; ?></h1>
+            <a href="<?php echo esc_url($movie['link']); ?>" style="color:black">
+                <h1><?php echo $movie['title']; ?></h1>
+            </a>
+        </div>
+        <div class="ten wide column"></div>
+    </div>
+    <div class="ui grid stackable">
+        <div class="five wide column">
+            <p>
+                <?php echo $movie['excerpt']; ?>
+            </p>
+            <a href="<?php echo esc_url($movie['link']); ?>" class="landing-info"><?php _e('More info','sage'); ?></a>
+            <br>
+            <br>
+            <h5>
+                <?php if ($movie['director']): ?>
+                    <?php _e('Director: ','sage'); ?>
+                    <?php foreach ($movie['director'] as $x) {
+                        echo $x->name . ' ';
+                    } ?>
+                    <br>
+                <?php endif; ?>
 
                 <?php if ($movie['countries']): ?>
-                  <?php _e('Country: ','sage'); ?>
-                  <?php foreach ($movie['country'] as $x) {
-                    echo $x->name . ' ';
-                  } ?>
-                  <br>
+                    <?php _e('Country: ','sage'); ?>
+                    <?php foreach ($movie['country'] as $x) {
+                        echo $x->name . ' ';
+                    } ?>
+                    <br>
                 <?php endif; ?>
 
                 <?php if ($movie['year']): ?>
-                  <?php _e('Year: ','sage'); ?>
-                  <?php foreach ($movie['year'] as $x) {
-                    echo $x->name . ' ';
-                  } ?>
-                  <br>
+                    <?php _e('Year: ','sage'); ?>
+                    <?php foreach ($movie['year'] as $x) {
+                        echo $x->name . ' ';
+                    } ?>
+                    <br>
                 <?php endif ?>
 
                 <?php if ($movie['language']): ?>
-                  <?php _e('Language: ','sage'); ?>
-                  <?php foreach ($movie['language'] as $x) {
-                    echo $x->name . ' ';
-                  } ?>
-                  <br>
+                    <?php _e('Language: ','sage'); ?>
+                    <?php foreach ($movie['language'] as $x) {
+                        echo $x->name . ' ';
+                    } ?>
+                    <br>
                 <?php endif ?>
 
                 <?php if ($movie['format']): ?>
-                  <?php _e('Format: ','sage'); ?>
-                  <?php foreach ($movie['format'] as $x) {
-                    echo $x->name . ' ';
-                  } ?>
-                  <br>
+                    <?php _e('Format: ','sage'); ?>
+                    <?php foreach ($movie['format'] as $x) {
+                        echo $x->name . ' ';
+                    } ?>
+                    <br>
                 <?php endif ?>
                 <?php if ($movie['duration']): ?>
-                  <?php _e('Duration: ','sage'); ?>
-                  <?php echo $movie['duration']; ?>
+                    <?php _e('Duration: ','sage'); ?>
+                    <?php echo $movie['duration']; ?>
                 <?php endif ?>
-              </h5>
-              <br><br>
-            </div>
-          </div>
-
-          <div class="col-md-1 col-lg-1"></div>
-
-          <div class="col-12 col-sm-12 col-md-7 col-lg-7">
-            @php( $trailer = get_post_meta($post->ID,'film_trailer',true) )
-            @if ($trailer)
-              <div class="movie-trailer ui embed" data-source="youtube" data-id="<?php echo $trailer; ?>" data-placeholder="<?php echo $movie['image']; ?>"></div>
-            @else
-              <a href="{{ esc_url($movie['link']) }}">
-                <img src="{{ $movie['image'] }}" alt="" class="img-fluid" />
-              </a>
-            @endif
-          </div>
+            </h5>
+            <br><br>
         </div>
-      </div>
+        <div class="one wide column"></div>
+        <div class="ten wide column">
+            <?php $trailer = get_post_meta($post->ID,'film_trailer',true); ?>
+            <?php if ($trailer): ?>
+                <div class="movie-trailer ui embed" data-source="youtube" data-id="<?php echo $trailer; ?>" data-placeholder="<?php echo $movie['image']; ?>"></div>
+            <?php else: ?>
+                <a href="<?php echo esc_url($movie['link']); ?>">
+                    <img src="<?php echo $movie['image']; ?>" alt="" class="ui image" />
+                </a>
+            <?php endif; ?>
+
+        </div>
+    </div>
+</div>
+<div class="one wide column"></div>
     @endforeach
   </div>
 @else
   <br>
-  <div class="row">
-    <div class="col-1"></div>
-    <div class="col-11">
-      <h1><?php _e('Nothing at Spoutnik for today!','sage'); ?></h1>
+    <div class="ui grid">
+        <div class="one wide column"></div>
+        <div class="ten wide column">
+            <h1><?php _e('Nothing at Spoutnik for today!','sage'); ?></h1>
+        </div>
     </div>
-  </div>
 @endif
 <br>
 <br>
