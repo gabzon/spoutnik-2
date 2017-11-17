2e57e603174629c4b0f7122971c0b3880595bf87
gabzon <gab.zambrano@gmail.com>
2017-11-17 14:41:27 +0100

    country , program taxonomy

--- resources/views/taxonomy-country.blade.php
+++ resources/views/taxonomy-country.blade.php
@@ -1,37 +1,40 @@
-<?php get_template_part('templates/page', 'header'); ?>
+@extends('layouts.app')
 
-@if ( !have_posts() )
-  <div class="alert alert-warning">
-      <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-@endif
+@section('content')
+  @include('partials.page-header')
 
-<div class="ui container">
+  @if (!have_posts())
+    <div class="alert alert-warning">
+      {{ __('Sorry, no results were found.', 'sage') }}
+    </div>
+    {!! get_search_form(false) !!}
+  @endif
+
+  <div class="ui container">
 
     @php( $image_country = get_term_meta(get_queried_object()->term_id, 'image_country', false) )
     <h1>{{ get_queried_object()->name; }}</h1>
     <div class="ui grid stackable">
         <div class="one wide column"></div>
         <div class="seven wide column">
-            <img src="<?= $image_country[0]; ?>" alt="" class="ui image" />
+            <img src="{{ $image_country[0] }}" alt="" class="ui image" />
         </div>
         <div class="seven wide column">
-            <?php echo term_description( $term_id, $taxonomy ) ?>
+            @php( echo term_description( $term_id, $taxonomy ) )
         </div>
         <div class="one wide column"></div>
     </div>
-</div>
-<br>
-<br>
-<br>
-<div class="ui container">
-    <div class="ui four column grid stackable">
-        <?php while (have_posts()) : the_post(); ?>
-            <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-        <?php endwhile; ?>
-    </div>
-    <?php the_posts_navigation(); ?>
     <br>
-</div>
+    <br>
+    <br>
+
+    <div class="ui four column grid stackable">
+      @while (have_posts()) @php(the_post())
+        @include('partials.content-'.get_post_type())
+      @endwhile
+    </div>
+    {!! get_the_posts_navigation() !!}
+    <br>
+  </div>
 <br>
+@endsection
