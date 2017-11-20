e24b76a117fca0baed1d1f27a32c636c9c76af76
gabzon <gab.zambrano@gmail.com>
2017-11-20 16:27:15 +0100

    functions template blog

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,15 +12,12 @@
 
   <div class="ui container">
     <div class="ui four column grid stackable">
-      <?php while (have_posts()) : the_post(); ?>
-        <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-      <?php endwhile; ?>
+      @while (have_posts()) @php(the_post())
+        @include('partials.content-'.get_post_type())
+      @endwhile
     </div>
   </div>
   <br>
-  @while (have_posts()) @php(the_post())
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
   {!! get_the_posts_navigation() !!}
+  <br>
 @endsection
