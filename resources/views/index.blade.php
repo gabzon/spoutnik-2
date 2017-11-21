baad590cc48594e3bdcca6bb921167b68e3f891a
gabzon <gab.zambrano@gmail.com>
2017-11-21 17:11:23 +0100

    posts working

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,7 @@
+
+
+
+
 @extends('layouts.app')
 
 @section('content')
--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -2,35 +2,32 @@
   <br>
   <br>
   <br>
-  @while ( have_posts() )
-    @php(the_post())
-
-    <article @php(post_class())>
-      <header>
-        <h1 class="entry-title">{{ get_the_title() }}</h1>
-        {{-- @include('partials/entry-meta') --}}
-      </header>
-      <br><br>
-      <div class="entry-content">
-        <div class="ui two column grid stackable">
-          <div class="column">
-            @php( $image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'full') )
-            <img src="{{ $image[0] }}" alt="" class="ui image"/>
-          </div>
-          <div class="column">
-            @php(the_content())
-          </div>
-        </div>
+  <br>
+<article @php(post_class())>
+  <header>
+    <h1 class="entry-title">{{ get_the_title() }}</h1>
+    {{-- @include('partials/entry-meta') --}}
+  </header>
+  <br><br>
+  <div class="entry-content">
+    <div class="ui two column grid stackable">
+      <div class="column">
+        @php( $image = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'full') )
+        <img src="{{ $image[0] }}" alt="" class="ui image"/>
       </div>
-      <br>
-      <br>
-      <footer>
-        {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-      </footer>
-      @php(comments_template('/partials/comments.blade.php'))
-    </article>
+      <div class="column">
+        @php(the_content())
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+  <footer>
+    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+  </footer>
+  @php(comments_template('/partials/comments.blade.php'))
+</article>
 
-  @endwhile
-  <br>
-  <br>
+<br>
+<br>
 </div>
