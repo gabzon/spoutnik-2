41799ba714b0c51cc53fb69f22e44e583c7f5182
gabzon <gab.zambrano@gmail.com>
2017-11-22 17:07:45 +0100

    country template

--- resources/views/taxonomy-country.blade.php
+++ resources/views/taxonomy-country.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
+  <br>
+  <br>
+  <br>
+  <br>
   @if (!have_posts())
     <div class="alert alert-warning">
       {{ __('Sorry, no results were found.', 'sage') }}
@@ -11,23 +13,23 @@
   @endif
 
   <div class="ui container">
-
-    @php( $image_country = get_term_meta(get_queried_object()->term_id, 'image_country', false) )
-    <h1>{{ get_queried_object()->name; }}</h1>
+    @php($image_country = get_term_meta(get_queried_object()->term_id, 'image_country', false))
+    <h1>{{ get_queried_object()->name }}</h1>
     <div class="ui grid stackable">
-        <div class="one wide column"></div>
-        <div class="seven wide column">
-            <img src="{{ $image_country[0] }}" alt="" class="ui image" />
-        </div>
-        <div class="seven wide column">
-            @php( echo term_description( $term_id, $taxonomy ) )
-        </div>
-        <div class="one wide column"></div>
+      <div class="one wide column"></div>
+      <div class="seven wide column">
+        <img src="{{ $image_country[0] }}" alt="" class="ui image" />
+      </div>
+      <div class="seven wide column">
+        @php
+        echo term_description( $term_id, $taxonomy );
+        @endphp
+      </div>
+      <div class="one wide column"></div>
     </div>
     <br>
     <br>
     <br>
-
     <div class="ui four column grid stackable">
       @while (have_posts()) @php(the_post())
         @include('partials.content-'.get_post_type())
@@ -36,5 +38,5 @@
     {!! get_the_posts_navigation() !!}
     <br>
   </div>
-<br>
+  <br>
 @endsection
