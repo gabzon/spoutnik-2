0fa4d25406a4cd4fa0d1a93555fa4401e689a877
gabzon <gab.zambrano@gmail.com>
2018-09-24 16:12:22 +0200

    notifications style in film template

--- resources/views/film/notifications.blade.php
+++ resources/views/film/notifications.blade.php
@@ -1,24 +1,17 @@
 @if ($notification_dates)
   @php($alerts = get_post_meta($post->ID,'film_notifications'))
-  <div class="ui icon red message">
-    <table>
-      @for ($i=0; $i < count($alerts[0]); $i++)
-        <tr>
-          <td style="width:50%">
-            <div class="header">
-              @php
-              $raw_date = $alerts[0][$i]['film_notification_date'];
-              $alert_date = DateTime::createFromFormat('d/m/Y', $raw_date)->format('Y-m-d');
-              $alert_date = new DateTime($alert_date);
-              @endphp
-              {{ utf8_encode(strftime("%a %e %b %G", $alert_date->getTimestamp())) }}
-            </div>
-          </td>
-          <td>
-            <p>{{ $alerts[0][$i]['film_notification_desc']}}</p>
-          </td>
-        </tr>
-      @endfor
-    </table>
+  <h4>{{ get_post_meta($post->ID, 'film_landing', true) }}</h4>
+  <div class="notifications">
+    @for ($i=0; $i < count($alerts[0]); $i++)
+      @php
+      $raw_date = $alerts[0][$i]['film_notification_date'];
+      $alert_date = DateTime::createFromFormat('d/m/Y', $raw_date)->format('Y-m-d');
+      $alert_date = new DateTime($alert_date);
+      $color_text = App::text_color();
+      @endphp
+      <h4 style="color:{{ $color_text }}">
+        {{ utf8_encode(strftime("%a %e %b %G", $alert_date->getTimestamp())) }}, {{ $alerts[0][$i]['film_notification_desc']}}
+      </h4>
+    @endfor
   </div>
 @endif
