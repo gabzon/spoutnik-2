e57e0f52a0488a2b68a70bd4b505f2b7244b390d
gabzon <gab.zambrano@gmail.com>
2018-09-27 16:34:03 +0200

    langue

--- resources/views/langues.blade.php
+++ resources/views/langues.blade.php
@@ -20,19 +20,18 @@ Template Name: Langues
           <img src="{{ get_the_post_thumbnail_url(get_the_ID()) }}" alt="" class="ui image" />
         </div>
         <div class="seven wide column">
-
           @include('partials.content-page')
         </div>
         <div class="one wide column"></div>
       </div>
-
-
-
+      <br>
+      <br>
+      <br>
       @php
       $args = array(
         'post_type'      => 'film',
         'posts_per_page'  => -1,
-        'posts_per_page' => 'publish',
+        // 'posts_per_page' => 'publish',
         'tax_query'      => array(
           array(
             'taxonomy' => 'language',
@@ -42,32 +41,50 @@ Template Name: Langues
           ),
         ),
       );
-
-
       // The Query
       $the_query = new WP_Query( $args );
-      //Piklist::pre($the_query);
-      //The Loop
-      if ( $the_query->have_posts() ) {
-        echo '<ol>';
-        while ( $the_query->have_posts() ) {
-          $the_query->the_post();
-          echo '<li>' . get_the_title() . '</li>';
-        }
-        echo '</ol>';
-        /* Restore original Post Data */
-        wp_reset_postdata();
-      } else {
-        echo 'merci';
-      }
       @endphp
+      <h1>Upcoming english speaking friendly films/events at Cinema Spoutnik</h1>
+      <br>
+      <div class="ui container">
+        <div class="ui four column grid stackable">
+          @if ( $the_query->have_posts() )
+            @while ($the_query->have_posts()) @php($the_query->the_post())
+              <div class="column">
+                <article @php(post_class())>
+                  <header>
+                    @php( $thumb = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()),'full') )
+                    <a href="@php(the_permalink())">
+                      <img src="{{ $thumb['0'] }}" alt="" class="ui image"/>
+                    </a>
+                    <h2 class="entry-title"><a href="@php(the_permalink())" class="title-link">@php( the_title() )</a></h2>
+                    @if ( get_post_meta($post->ID,'film_landing',true) )
+                      <h4>
+                        @php
+                          echo get_post_meta($post->ID,'film_landing',true);
+                        @endphp
+                      </h4>
+                    @endif
+                  </header>
+                  <br>
+                  <div class="entry-summary">
+                    @php(the_excerpt())
+                  </div>
+                </article>
+              </div>
+            @endwhile
+            @php
+              wp_reset_postdata();
+            @endphp
+          @else
+            Nous n'avons pas de films
+          @endif
 
-
-
-
+        </div>
+      </div>
+      {!! get_the_posts_navigation() !!}
     </div>
   @endwhile
   <br>
   <br>
-
 @endsection
