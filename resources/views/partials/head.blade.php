a2794b811b906777a2a010ee70f6723bb27c455d
gabzon <gab.zambrano@gmail.com>
2019-10-28 11:55:24 +0100

    fonts/sidebar/head

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,16 +5,4 @@
   @php(wp_head())
   <link rel="stylesheet" href="https://unpkg.com/tachyons@4.8.1/css/tachyons.min.css"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css">
-  <style type="text/css">
-  @font-face {
-    font-family: 'suisseIntl';
-    src: url(https://spoutnik.info/wp-content/themes/spoutnik-2/resources/assets/fonts/SuisseIntl-Medium-WebXL.eot);
-    src: url(https://spoutnik.info/wp-content/themes/spoutnik-2/resources/assets/fonts/SuisseIntl-Medium-WebXL.woff2) format('woff'), url(https://spoutnik.info/wp-content/themes/spoutnik-2/resources/assets/fonts/SuisseIntl-Medium-WebXL.woff) format('woff'), url(https://spoutnik.info/wp-content/themes/spoutnik-2/resources/assets/fonts/SuisseIntl-Medium-WebXL.ttf) format('truetype'), url(https://spoutnik.info/wp-content/themes/spoutnik-2/resources/assets/fonts/SuisseIntl-Medium-WebXL.svg) format('svg');
-    font-style: normal;
-    font-weight: normal;
-    font-variant: normal;
-    text-decoration: inherit;
-    text-transform: none;
-  }
-  </style>
 </head>
--- resources/views/partials/sidebar-menu.blade.php
+++ resources/views/partials/sidebar-menu.blade.php
@@ -21,188 +21,189 @@ $horaire = $theme_options['spoutnik_horaire'];
 
 
 <div id="sidebar-computer-menu" class="ui right very wide sidebar vertical menu computer only" style="background:<?php echo $colorpicker; ?>; color:#fefefe;">
-    <div class="sidebar-container" style="margin:0 30px">
-        <br>
-        <br>
-        <div class="ui fluid accordion">
+  <div class="sidebar-container" style="margin:0 30px">
+    <br>
+    <br>
+    <div class="ui fluid accordion">
 
-            <div class="title active">
-                <h5 class="white"><i class="dropdown icon"></i>PROGRAMME</h5>
-            </div>
-            <div class="content active">
-                <p class="transition visible" style="display: block !important;">
-                    @php(dynamic_sidebar('sidebar-primary'))
-                </p>
-            </div>
-            <div class="title">
-                <h5 class="white"><i class="dropdown icon"></i><?php _e('TARIFS','sage') ?></h5>
-            </div>
-            <div class="content">
-                <h5 class="transition hidden sidebar-h5">
-                    <table>
-                        <tr>
-                            <td><?php _e('normal ','sage'); ?></td>
-                            <td>{{ $normal }}</td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('reduction ','sage'); ?></td>
-                            <td><?php echo $reduit; ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('infant ','sage'); ?></td>
-                            <td><?php echo $enfant; ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('20years/20francs','sage'); ?></td>
-                            <td>Fr.5.– </td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('member ','sage'); ?></td>
-                            <td>
-                                <?php echo $membre . ' ('. $membre_annee .')'; ?>
-                                <br>
-                                Amorti en 5 séances !
-                            </td>
-                        </tr>
-                    </table>
-                    <br>
-                    <?php echo _e('En vente à la billetterie du Spoutnik','sage') ?>
-                </h5>
-            </div>
-            <div class="title">
-                <h5 class="white"><i class="dropdown icon"></i><?php _e('CONTACT','sage'); ?></h5>
-            </div>
-            <div class="content">
-                <p class="transition hidden">
-                    <h5 class="sidebar-h5">
-                        <?php _e('Cinéma Spoutnik','sage'); ?><br>
-                        <?php echo $cinema_address; ?><br><br>
-                        <?php echo $google_maps_cinema; ?><br><br>
-                        <?php _e('Spoutnik Office','sage'); ?><br>
-                        <?php echo $office_address; ?><br><br>
-                        <i class="mail icon"></i> <?php echo $email; ?><br>
-                        <i class="phone icon"></i> <?php echo $phone; ?><br>
-                        <br>
-                        <?php _e('Opening hours','sage'); ?><br>
-                        <?php if ($horaire): ?>
-                            <?php echo $horaire ?>
-                        <?php else: ?>
-                            <?php _e('Irregular schedule','sage'); ?>
-                        <?php endif; ?>
-                    </h5>
+      <div class="title active">
+        <h5 class="white"><i class="dropdown icon"></i>PROGRAMME</h5>
+      </div>
+      <div class="content active">
+        <p class="transition visible" style="display: block !important;">
+          @php(dynamic_sidebar('sidebar-primary'))
+        </p>
+      </div>
+      <div class="title">
+        <h5 class="white"><i class="dropdown icon"></i><?php _e('TARIFS','sage') ?></h5>
+      </div>
+      <div class="content">
+        <h5 class="transition hidden sidebar-h5">
+          <table>
+            <tr>
+              <td><?php _e('normal ','sage'); ?></td>
+              <td>{{ $normal }}</td>
+            </tr>
+            <tr>
+              <td><?php _e('reduction ','sage'); ?></td>
+              <td><?php echo $reduit; ?></td>
+            </tr>
+            <tr>
+              <td><?php _e('infant ','sage'); ?></td>
+              <td><?php echo $enfant; ?></td>
+            </tr>
+            <tr>
+              <td><?php _e('20years/20francs','sage'); ?></td>
+              <td>Fr.5.– </td>
+            </tr>
+            <tr>
+              <td><?php _e('member ','sage'); ?></td>
+              <td>
+                <?php echo $membre . ' ('. $membre_annee .')'; ?>
+                <br>
+                Amorti en 5 séances !
+              </td>
+            </tr>
+          </table>
+          <br>
+          <?php echo _e('En vente à la billetterie du Spoutnik','sage') ?>
+        </h5>
+      </div>
+      <div class="title">
+        <h5 class="white"><i class="dropdown icon"></i><?php _e('CONTACT','sage'); ?></h5>
+      </div>
+      <div class="content">
+        <p class="transition hidden">
+          <h5 class="sidebar-h5">
+            <?php _e('Cinéma Spoutnik','sage'); ?><br>
+            <?php echo $cinema_address; ?><br><br>
+            <?php echo $google_maps_cinema; ?><br><br>
+            <?php _e('Spoutnik Office','sage'); ?><br>
+            <?php echo $office_address; ?><br><br>
+            <i class="mail icon"></i> <?php echo $email; ?><br>
+            <i class="phone icon"></i> <?php echo $phone; ?><br>
+            <br>
+            <?php _e('Opening hours','sage'); ?><br>
+            <?php if ($horaire): ?>
+              <?php echo $horaire ?>
+            <?php else: ?>
+              <?php _e('Irregular schedule','sage'); ?>
+            <?php endif; ?>
+          </h5>
 
-                    <h5 class="sidebar-h5">
-                        <?php _e('ORGANISERS: ','sage'); ?><br><br>
-                        <?php echo $organisers; ?>
-                    </h5>
+          <h5 class="sidebar-h5">
+            <?php _e('ORGANISERS: ','sage'); ?><br><br>
+            <?php echo $organisers; ?>
+          </h5>
 
-                    <h5 class="sidebar-h5"><?php _e('THE COMMITTEE:','sage'); ?>
-                    <?php echo $committee; ?>
-                    </h5>
-                </p>
-            </div>
-            <div class="title">
-                <h5 class="white"><i class="dropdown icon"></i><?php _e('INFO','sage'); ?></h5>
-            </div>
-            <div class="content">
-                <h5 class="transition hidden sidebar-h5">
-                    <?php echo $info; ?>
-                </h5>
-            </div>
-        </div>
+          <h5 class="sidebar-h5"><?php _e('THE COMMITTEE:','sage'); ?>
+            <?php echo $committee; ?>
+          </h5>
+        </p>
+      </div>
+      <div class="title">
+        <h5 class="white"><i class="dropdown icon"></i><?php _e('INFO','sage'); ?></h5>
+      </div>
+      <div class="content">
+        <h5 class="transition hidden sidebar-h5">
+          <?php echo $info; ?>
+        </h5>
+      </div>
     </div>
+  </div>
 </div>
 
 <div id="sidebar-mobile-menu" class="ui right wide sidebar vertical menu mobile only" style="background:<?php echo $colorpicker; ?>; color:#fefefe;">
 
-    <div class="sidebar-container" style="margin:0 30px">
-        <br>
-        <br>
-        <div class="ui fluid accordion">
+  <div class="sidebar-container" style="margin:0 30px">
+    <br>
+    <br>
+    <div class="ui fluid accordion">
 
-            <div class="title active">
-                <h5><i class="dropdown icon"></i>PROGRAMME</h5>
-            </div>
-            <div class="content active">
-                <p class="transition visible" style="display: block !important;">
-                    @php(dynamic_sidebar('sidebar-primary'))
-                </p>
-            </div>
-            <div class="title">
-                <h5 style="text-transform:uppercase;"><i class="dropdown icon"></i><?php _e('TARIFS','sage') ?></h5>
-            </div>
-            <div class="content">
-                <h5 class="transition hidden sidebar-h5">
-                    <table>
-                        <tr>
-                            <td><?php _e('normal ','sage'); ?></td>
-                            <td><?php echo $normal; ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('reduction ','sage'); ?></td>
-                            <td><?php echo $reduit; ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('infant ','sage'); ?></td>
-                            <td><?php echo $enfant; ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('20years/20francs','sage'); ?></td>
-                            <td>Fr.5.– </td>
-                        </tr>
-                        <tr>
-                            <td><?php _e('member ','sage'); ?></td>
-                            <td>
-                                <?php echo $membre . ' ('. $membre_annee .')'; ?>
-                                <br>
-                                Amorti en 5 séances !
-                            </td>
-                        </tr>
-                    </table>
-                    <br>
-                    <?php echo _e('En vente à la billetterie du Spoutnik','sage') ?>
-                </h5>
-            </div>
-            <div class="title">
-                <h5><i class="dropdown icon"></i><?php _e('CONTACT','sage'); ?></h5>
-            </div>
-            <div class="content">
-                <p class="transition hidden">
-                    <h5 class="sidebar-h5">
-                        <?php _e('Cinéma Spoutnik','sage'); ?><br>
-                        <?php echo $cinema_address; ?><br><br>
-                        <?php echo $google_maps_cinema; ?><br><br>
-                        <?php _e('Spoutnik Office','sage'); ?><br>
-                        <?php echo $office_address; ?><br><br>
-                        <i class="mail icon"></i> <?php echo $email; ?><br>
-                        <i class="phone icon"></i> <?php echo $phone; ?><br>
-                        <br>
-                        <?php _e('Opening hours','sage'); ?><br>
-                        <?php if ($horaire): ?>
-                            <?php echo $horaire ?>
-                        <?php else: ?>
-                            <?php _e('Irregular schedule','sage'); ?>
-                        <?php endif; ?>
-                    </h5>
+      <div class="title active">
+        <h5><i class="dropdown icon"></i>PROGRAMME</h5>
+      </div>
+      <div class="content active">
+        <p class="transition visible" style="display: block !important;">
+          @php(dynamic_sidebar('sidebar-primary'))
+        </p>
+      </div>
+      <div class="title">
+        <h5 style="text-transform:uppercase;"><i class="dropdown icon"></i><?php _e('TARIFS','sage') ?></h5>
+      </div>
+      <div class="content">
+        <h5 class="transition hidden sidebar-h5">
+          <table>
+            <tr>
+              <td><?php _e('normal ','sage'); ?></td>
+              <td><?php echo $normal; ?></td>
+            </tr>
+            <tr>
+              <td><?php _e('reduction ','sage'); ?></td>
+              <td><?php echo $reduit; ?></td>
+            </tr>
+            <tr>
+              <td><?php _e('infant ','sage'); ?></td>
+              <td><?php echo $enfant; ?></td>
+            </tr>
+            <tr>
+              <td><?php _e('20years/20francs','sage'); ?></td>
+              <td>Fr.5.– </td>
+            </tr>
+            <tr>
+              <td><?php _e('member ','sage'); ?></td>
+              <td>
+                <?php echo $membre . ' ('. $membre_annee .')'; ?>
+                <br>
+                Amorti en 5 séances !
+              </td>
+            </tr>
+          </table>
+          <br>
+          <?php echo _e('En vente à la billetterie du Spoutnik','sage') ?>
+        </h5>
+      </div>
+      <div class="title">
+        <h5><i class="dropdown icon"></i><?php _e('CONTACT','sage'); ?></h5>
+      </div>
+      <div class="content">
+        <p class="transition hidden">
+          <h5 class="sidebar-h5">
+            <?php _e('Cinéma Spoutnik','sage'); ?><br>
+            <?php echo $cinema_address; ?><br><br>
+            <?php echo $google_maps_cinema; ?><br><br>
+            <?php _e('Spoutnik Office','sage'); ?><br>
+            <?php echo $office_address; ?><br><br>
+            <i class="mail icon"></i> <?php echo $email; ?><br>
+            <i class="phone icon"></i> <?php echo $phone; ?><br>
+            <br>
+            <?php _e('Opening hours','sage'); ?><br>
+            <?php if ($horaire): ?>
+              <?php echo $horaire ?>
+            <?php else: ?>
+              <?php _e('Irregular schedule','sage'); ?>
+            <?php endif; ?>
+          </h5>
 
-                    <h5 class="sidebar-h5">
-                        <?php _e('ORGANISERS: ','sage'); ?><br><br>
-                        <?php echo $organisers; ?>
-                    </h5>
+          <h5 class="sidebar-h5">
+            <?php _e('ORGANISERS: ','sage'); ?><br><br>
+            <?php echo $organisers; ?>
+          </h5>
 
-                    <h5><?php _e('THE COMMITTEE:','sage'); ?></h5>
-                    <?php echo $committee; ?>
-
-                </p>
-            </div>
-            <div class="title">
-                <h5><i class="dropdown icon"></i><?php _e('INFO','sage'); ?></h5>
-            </div>
-            <div class="content">
-                <h5 class="transition hidden sidebar-h5">
-                    <?php echo $info; ?>
-                </h5>
-            </div>
-        </div>
+          <h5 class="sidebar-h5">
+            <?php _e('THE COMMITTEE:','sage'); ?><br><br>
+            <?php echo $committee; ?>
+          </h5>
+        </p>
+      </div>
+      <div class="title">
+        <h5><i class="dropdown icon"></i><?php _e('INFO','sage'); ?></h5>
+      </div>
+      <div class="content">
+        <h5 class="transition hidden sidebar-h5">
+          <?php echo $info; ?>
+        </h5>
+      </div>
     </div>
+  </div>
 </div>
