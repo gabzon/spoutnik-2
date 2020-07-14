c173c5abf8fa0cc9920c7e51f6a252e960fb4910
Gabriel Zambrano <gab.zambrano@gmail.com>
2020-07-14 13:51:17 +0200

    display collaboration page

---
+++ resources/piklist/parts/terms/collaboration.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Title: Extra options
+Taxonomy: cycle
+Capability: manage_options
+*/
+
+// Let's create a text box field
+ piklist('field', [
+     'type'     => 'text',
+     'field'    => 'image_collaboration',
+     'label'    => __('Add image link URL'),
+     'help'     => __('Image should have a width bigger than 980px'),
+     'columns'  => 12
+ ]);
+
+ ?>
---
+++ resources/views/taxonomy-collaboration.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+{{-- @include('partials.page-header') --}}
+<br>
+<br>
+<br>
+<br>
+@if (!have_posts())
+<div class="alert alert-warning">
+    {{ __('Sorry, no results were found.', 'sage') }}
+</div>
+{!! get_search_form(false) !!}
+@endif
+
+<div class="ui container">
+    <?php $image_collaboration = get_term_meta(get_queried_object()->term_id, 'image_collaboration', true); ?>
+    <h1>{{ get_queried_object()->name }}</h1>
+    <div class="ui grid stackable">
+        <div class="one wide column"></div>
+        <div class="seven wide column">
+            <img src="{{ $image_collaboration }}" alt="" class="ui image" />
+        </div>
+        <div class="seven wide column">
+            @php
+            echo term_description( $term_id, $taxonomy );
+            @endphp
+        </div>
+        <div class="one wide column"></div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<div class="ui container">
+    @php
+    //global $wp_query;
+    //$loaded_posts = $wp_query->posts;
+    //array_reverse($loaded_posts);
+    // $wp_the_query->posts = $loaded_posts;
+    @endphp
+    <div class="ui four column grid stackable">
+        @while (have_posts()) @php(the_post())
+        @include('partials.content-'.get_post_type())
+        @endwhile
+    </div>
+</div>
+{!! get_the_posts_navigation() !!}
+<br>
+
+@endsection
